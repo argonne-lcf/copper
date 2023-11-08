4afd28e0dd43b04096d580a5cec90c1f813f1720
NotAstronaut <85028448+NotAstronaut@users.noreply.github.com>
2023-11-08 15:39:29 -0600

    .

--- src/testcase.c
+++ src/testcase.c
@@ -27,17 +27,19 @@ char* fileName;
 static int fuse_getattr(const char *path, struct stat *stbuf, struct fuse_file_info *fi) {
     int res = 0;
     memset(stbuf, 0, sizeof(struct stat));
+
     if (strcmp(path, "/") == 0) {
         stbuf->st_mode = S_IFDIR | 0755;
         stbuf->st_nlink = 2;
+
     } else if (strcmp(path, ((FileCheck*)fi->fh)->filename) == 0) {
         stbuf->st_mode = S_IFREG | 0444;
         stbuf->st_nlink = 1;
         stbuf->st_size = strlen("Hello World!\n");
         ((FileCheck*)fi->fh)->exists = true;
-    } else
+    } else {
         res = -ENOENT;
-
+    }
     return res;
 }
 
@@ -55,7 +57,7 @@ bool doesFileExist(char* targetFilename, char* nodePath) {
 }
 
 char* thisNode() {
-    //NOTE This method will be different, requiring looking into the system info
+    //NOTE This method will be different, requiring looking into the system info of the node
     return "Hello";
 }
 
