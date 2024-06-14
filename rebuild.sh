c27f441a78471f8fa9be06db694b9e94f7c0706f
TheAssembler1 <lewisnoah592@gmail.com>
2024-06-14 14:13:58 -0500

    fix CMAKELists and create build dir if not found rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,11 @@
+if [ -z build ]
+then
+	echo "found build dir"
+else 
+	echo "creating build dir"
+	mkdir build
+fi
+
 cd build || { echo "Failed to move to build dir"; exit 1; }
 make || { echo "Failed to build mongo_fuse_fs"; exit 1; }
 
