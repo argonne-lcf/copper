75dc0566d9e986ae42913de21a6830a4e55098d0
TheAssembler1 <lewisnoah592@gmail.com>
2024-06-18 09:33:43 -0500

    use view_dir env var when ioctl'ing

--- ioctl.py
+++ ioctl.py
@@ -2,9 +2,9 @@ import fcntl
 import os
 import struct
 
-print(os.environ['HOME'])
-cur_dir = os.getcwd();
-fd = os.open(cur_dir + "/../" + "view" + "/file1", os.O_RDWR)
+view_dir = (os.environ['VIEW_DIR'])
+cur_dir = os.getcwd()
+fd = os.open(view_dir + "/file1", os.O_RDWR)
 IOCTL_REQUEST_CODE = 0
 result = fcntl.ioctl(fd, IOCTL_REQUEST_CODE, 0)
 os.close(fd)
