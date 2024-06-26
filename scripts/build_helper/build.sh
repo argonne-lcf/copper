b91fae66c158d64553f8f00df9196ce7edae4825
TheAssembler1 <lewisnoah592@gmail.com>
2024-06-26 18:22:46 -0500

    combine rebuild and remake to build.sh

--- scripts/build_helper/build.sh
+++ scripts/build_helper/build.sh
@@ -19,6 +19,10 @@ else
 	mkdir build
 fi
 
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -Bbuild || { echo "Failed to create compile commands"; exit 1; }
+cp build/compile_commands.json . || { echo "Failed to copy compile commands"; exit 1; }
+
+
 cd build || { echo "Failed to move to build dir"; exit 1; }
 make || { echo "Failed to build mongo_fuse_fs"; exit 1; }
 
