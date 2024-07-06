17ecad0a62b81a0c01eb252dcf9c20f89adc62c3
TheAssembler1 <lewisnoah592@gmail.com>
2024-07-06 06:01:05 -0500

    auto metric collection and test scripts

--- scripts/test/run_tests.sh
+++ scripts/test/run_tests.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #set -o nounset
 
-ITERATIONS=1
+# SET TO WHAT SCRIPTS YOU WANT TO TEST
+#   Options:
+#     1. view
+#     2. target
+#     3. view_and_target
+export WHAT_TO_TEST=target
 
 SCRIPT=$(realpath -s "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
@@ -15,12 +20,31 @@ fi
 
 source $ENV
 
-export JOB_ID=$(shuf -i 1-9999999 -n 1)
-mkdir "$(pwd)/job_${JOB_ID}"
-export JOB_OUTPUT_DIR="$(pwd)/job_${JOB_ID}"
+export TEST_ID=$(shuf -i 1-9999999 -n 1)
+export HOSTNAME=$(hostname)
 
-module use /soft/preview-modulefiles/24.086.0
-module load frameworks/2024.04.15.002
+export TEST_TOP_DIR=$SCRIPTS_DIR/test/test_output
+export TEST_ID_TOP_DIR=$TEST_TOP_DIR
+export TEST_OUTPUT_DIR="$TEST_ID_TOP_DIR/$(hostname)/${TEST_ID}"
+export TEST_OUTPUT_VIEW_DIR=$TEST_OUTPUT_DIR/view
+export TEST_OUTPUT_TARGET_DIR=$TEST_OUTPUT_DIR/target
 
-python3 driver.py $ITERATIONS | tee "$JOB_OUTPUT_DIR/driver_output.stdout"
+export TEST_TIME_FILENAME=output_time.csv
+export TEST_TIME_VIEW_PATH=$TEST_OUTPUT_VIEW_DIR/$TEST_TIME_FILENAME
+export TEST_TIME_TARGET_PATH=$TEST_OUTPUT_TARGET_DIR/$TEST_TIME_FILENAME
+
+echo "removing prev job output"
+rm -r $TEST_OUTPUT_DIR
+
+mkdir -p $TEST_OUTPUT_DIR
+mkdir -p $TEST_OUTPUT_VIEW_DIR
+mkdir -p $TEST_OUTPUT_TARGET_DIR
+
+export VIEW_SCRIPT_PATH=$SCRIPTS_DIR/test/view_test.py
+export TARGET_SCRIPT_PATH=$SCRIPTS_DIR/test/target_test.py
+
+# module use /soft/preview-modulefiles/24.086.0
+# module load frameworks/2024.04.15.002
+
+python3 driver.py | tee "$TEST_OUTPUT_DIR/driver_output.stdout"
 
--- scripts/test/target_test.py
+++ scripts/test/target_test.py
@@ -4,9 +4,7 @@ import os
 print("target_test script")
 
 packages_dir = os.environ['PY_PACKAGES_DIR']
-
 print(f"packages dir: {packages_dir}")
-
 sys.path.insert(0, packages_dir)
 
 import numpy
