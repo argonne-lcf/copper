f830ce05cbb737c63de106ffdedeeac15fd2cf26
Noah Lewis <47840925+TheAssembler1@users.noreply.github.com>
2024-08-14 18:57:34 -0500

    Create horovod_run.sh

---
+++ ae-ad-copper/test/horovod_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+module load frameworks
+
+WCOLL=$PBS_NODEFILE pdsh -t 60 $CMD
+nodes=$(cat $PBS_NODEFILE | wc -l)
+sleep 60
+
+echo "Launch Baseline Nodes:$nodes" 1>&2
+time mpiexec -n $((nodes*12)) -ppn 12 sleep 1
+
+echo "First Nodes:$nodes Python:12 ES:1 fcores:1 total:2" 1>&2
+time mpiexec -n $((nodes*12)) -ppn 12 \
+  --genvall \
+  --genv=PYTHONPATH=/tmp/copper/lus/flare/projects/Aurora_deployment/kaushik/copper/july12/copper/run/copper_conda_env \
+  --cpu-bind=list:4-51 \
+  python3 -c "import torch; import horovod.torch as hvd; print(hvd.__file__)"
